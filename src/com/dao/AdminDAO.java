5df025940b0f556eb05766a01bad8a6ff9fb6fb7
Siddhanta Bhattacharjee <65542550+Siddhanta836@users.noreply.github.com>
2022-09-07 17:45:33 +0530

    update
    update

--- src/com/dao/AdminDAO.java
+++ src/com/dao/AdminDAO.java
@@ -11,7 +11,7 @@ public class AdminDAO {
 	
 	public void displayRecords(Connection connection) throws SQLException
 	{
-		String sql = "select id, name, age, salary from Admin";
+		String sql = "select id, name, username, password from Admin";
 		Statement stmpt = connection.createStatement();
 		
 		ResultSet resultObj = stmpt.executeQuery(sql);
@@ -20,14 +20,14 @@ public class AdminDAO {
 		{
 			System.out.println(String.format("ID: %d", resultObj.getInt("id")));
 			System.out.println(String.format("Name: %s", resultObj.getString("name")));
-			System.out.println(String.format("Age: %d", resultObj.getInt("age")));
-			System.out.println(String.format("Salary: %d", resultObj.getInt("salary")));
+			System.out.println(String.format("Username: %d", resultObj.getInt("username")));
+			System.out.println(String.format("Password: %d", resultObj.getInt("password")));
 		}
 	}
 	
 	public void createTable(Connection connection) throws SQLException
 	{
-		String sql = "create table Employee (id int, name varchar(30), age int, salary int)";
+		String sql = "create table Employee (id int, name varchar(30), username varchar(30),password varchar(30))";
 		Statement stmt = connection.createStatement();
 		
 		stmt.execute(sql);
@@ -35,25 +35,24 @@ public class AdminDAO {
 	
 	public int insertData(Connection connection, Admin e) throws SQLException
 	{
-		String sql = "insert into Employee (id, name, age, salary) values (?,?,?,?)";
+		String sql = "insert into Employee (id, name, username, password) values (?,?,?,?)";
 		PreparedStatement pstmt = connection.prepareStatement(sql);
 		pstmt.setInt(1, e.getId());
 		pstmt.setString(2, e.getName());
-		pstmt.setInt(3, e.getAge());
-		pstmt.setInt(4, e.getSalary());
+		pstmt.setInt(3, e.getUsername());
+		pstmt.setInt(4, e.getPassword());
 		
 		int rows=pstmt.executeUpdate();
 		return rows;
 	}
 	
-	public int updateData(Connection connection,String name,int age,int sal,int id) throws SQLException
+	public int updateData(Connection connection,String name,String username,String password,int id) throws SQLException
 	{
 		String sql="update Employee set name=?,age=?,salary=? where id=?";
 		PreparedStatement ps=connection.prepareStatement(sql);
 		ps.setString(1,name);
-		ps.setInt(2,age);
-		ps.setInt(3, sal);
-		ps.setInt(4, id);
+		ps.setInt(2,username);
+		ps.setInt(3, password);
 		int updated=ps.executeUpdate();
 		return updated;
 		
