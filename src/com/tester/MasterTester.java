0e8e2929c22570b9f47dcf7bbda68659e87b32b0
Siddhanta Bhattacharjee <65542550+Siddhanta836@users.noreply.github.com>
2022-09-08 16:55:23 +0530

    update
    update

--- src/com/tester/MasterTester.java
+++ src/com/tester/MasterTester.java
@@ -1,12 +1,12 @@
 package com.tester;
-
+import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import com.bean.Admin;
-import com.dao.AdminDAO;
+import com.bean.*;
+import com.dao.*;
 
 public class MasterTester {
 
@@ -19,63 +19,62 @@ public class MasterTester {
 		System.out.println("The connection is established");
 		
 		AdminDAO dao=new AdminDAO();
+		dao.createTable(connection);
+		Admin df = new Admin(101,"default","default101","12345");
+		int a=dao.insertData(connection,df);
+		if(a==1){
+			System.out.println("Default admin added Successfully");
+		}
 		Scanner sc = new Scanner(System.in);
-		
-		//dao.createTable(connection);
-		
-		while(true)
-		{
-			System.out.println("Menu");
-			System.out.println("1. Display\n2. Insert\n3. Delete\n4. Update");
-			
-			switch(Integer.parseInt(sc.nextLine()))
-			{
-				case 1: 
-					dao.displayRecords(connection);
+		System.out.println("please verify your identity\nusername: ");
+		String user = sc.nextLine();
+		System.out.println("password: ");
+		String pass = sc.nextLine();
+		ResultSet rs = displayRecords(connection);
+		boolean auth = false;
+		while(rs.next()){
+			if((user.equalsIgnoreCase(rs.getString("username"))) && (pass.equals(rs.getString("password")))){
+				System.out.println("Acess granted!");
+				auth= true;
 				break;
-				
-				case 2: 
-					System.out.print("Enter your ID: ");
-					int id = Integer.parseInt(sc.nextLine());
-					System.out.print("\nEnter your Name: ");
-					String name = sc.nextLine();
-					System.out.print("\nEnter your Age: ");
-					String username = sc.nextLine();
-					System.out.print("\nEnter your Username: ");
-					String password = sc.nextLine();
-					System.out.print("\nEnter your Password: ");
-					Admin e1=new Admin(id,name,username,password);
-					System.out.println(dao.insertData(connection,e1));
-				break;
-				
-				case 3:
-					System.out.print("Enter your ID: ");
-					id = Integer.parseInt(sc.nextLine());
-					dao.deleteData(connection, id);
-				break;	
-				case 4:
-					
-					System.out.print("\nEnter your Name: ");
-					String n= sc.nextLine();
-					System.out.print("\nEnter your Age: ");
-					int a = Integer.parseInt(sc.nextLine());
-					System.out.print("\nEnter your Salary: ");
-					int sal = Integer.parseInt(sc.nextLine());
-					System.out.print("Enter your ID: ");
-					int eid = Integer.parseInt(sc.nextLine());
-					
-					dao.updateData(connection,n,a,sal,eid);
+			}
+		}
+		if(auth){
+			//switch statements
+			while(true){
+				System.out.println("Choose the operation you want to perform");
+				System.out.println("1. Edit Customer Data\n2. Edit Operator data\n3. Edit Retailer data\n4. Edit Admin data");
+				int c = sc.nextInt();
+				switch(c){
+					case 1:
+					System.out.println("Chose the action you want to perform on customers data:");
+					System.out.println("1. Add\n2. Update\n3. Delete\n4. Display");
 					break;
-					
-				
-					
+					case 2:
+					System.out.println("Chose the action you want to perform on operator data:");
+					System.out.println("1. Add\n2. Update\n3. Delete\n4. Display");
+					break;
+					case 3:
+					System.out.println("Chose the action you want to perform on retailer data:");
+					System.out.println("1. Add\n2. Update\n3. Delete\n4. Display");
+					break;
+					case 4:
+					System.out.println("Chose the action you want to perform on admin data:");
+					System.out.println("1. Add\n2. Update\n3. Delete\n4. Display");
+					break;
+					default:
+					System.out.println("Invalid Entry");
+					break;
+						
+					System.out.println("Do you want to continue(Y/N)");
+					char exit = sc.next();
+					if(exit=='Y'){
+						break;
+					}		
 			}
-			
-			System.out.println("Press e to exit!");
-			if(sc.nextLine().equals("e"))
-			{
-				break;
-			}
+		}
+		else{
+			System.out.println("Access denied");
 		}
 		
 		connection.close();
